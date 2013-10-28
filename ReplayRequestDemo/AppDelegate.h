e23c269b93565ba75520b22fecff46aaf3fb3628
Thibaut LE LEVIER <thibaut@lelevier.fr>
2013-10-28 23:01:51 +0100

    Initial Commit

---
+++ ReplayRequestDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReplayRequestDemo
+//
+//  Created by Thibaut LE LEVIER on 28/10/2013.
+//  Copyright (c) 2013 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReplayRequestDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReplayRequestDemo
+//
+//  Created by Thibaut LE LEVIER on 28/10/2013.
+//  Copyright (c) 2013 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
